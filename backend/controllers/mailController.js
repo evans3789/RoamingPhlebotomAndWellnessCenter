08404490bc98f2a9c6112769b486c13366d061e5
Evans Ondieki Nyakundi <108281203+evans3789@users.noreply.github.com>
2025-12-31 17:01:37 +0300

    Update mail transport configuration to use SMTP

--- backend/controllers/mailController.js
+++ backend/controllers/mailController.js
@@ -2,7 +2,8 @@ const nodemailer = require("nodemailer");
 
 // Configure transport
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
