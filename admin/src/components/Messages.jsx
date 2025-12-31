1812444348c6312714f1aa36689c91f446cd1c82
Evans Ondieki Nyakundi <108281203+evans3789@users.noreply.github.com>
2025-12-31 16:24:20 +0300

    Update API endpoint for fetching contacts

--- admin/src/components/Messages.jsx
+++ admin/src/components/Messages.jsx
@@ -11,7 +11,7 @@ const Messages = () => {
     const fetchContacts = async () => {
       const token = localStorage.getItem("token"); // admin JWT
       try {
-        const res = await axios.get("http://localhost:4000/api/contact", {
+        const res = await axios.get("https://roamingphlebotomandwellnesscenter-backend.onrender.com/api/contact", {
           headers: { Authorization: `Bearer ${token}` },
         });
         setContacts(res.data);
